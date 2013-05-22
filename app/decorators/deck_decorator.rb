d199d27c1ce43451d06ea201dba0e1ceed172dc7
David Ratajczak <david@mockra.com>
2013-05-21 21:52:13 -0700

    adds links partial with styling

--- app/decorators/deck_decorator.rb
+++ app/decorators/deck_decorator.rb
@@ -23,7 +23,7 @@ class DeckDecorator < Draper::Decorator
 
   def edit_link
     h.link_to h.edit_user_deck_path(user, self) do
-      h.content_tag 'i', nil, class: 'icon-edit-sign'
+      h.content_tag 'i', nil, class: 'icon-edit-sign icon-2x'
     end
   end
 end
