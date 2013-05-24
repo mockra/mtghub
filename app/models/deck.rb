108f3060d1a2ddda36a0bd2195a5c61a800d20a1
David Ratajczak <david@mockra.com>
2013-05-23 19:29:49 -0700

    adds dependent: :destroy to deck/sideboard.cards

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -3,7 +3,7 @@ class Deck < ActiveRecord::Base
   has_many :forks, class_name: 'Deck', foreign_key: 'origin_id'
   belongs_to :origin, class_name: 'Deck'
   has_many :cards_decks
-  has_many :cards, through: :cards_decks
+  has_many :cards, through: :cards_decks, dependent: :destroy
   has_one :sideboard, dependent: :destroy
 
   validates :title, presence: true
--- app/models/sideboard.rb
+++ app/models/sideboard.rb
@@ -2,5 +2,5 @@ class Sideboard < ActiveRecord::Base
   belongs_to :deck
   validates :deck_id, presence: true
   has_many :cards_sideboards
-  has_many :cards, through: :cards_sideboards
+  has_many :cards, through: :cards_sideboards, dependent: :destroy
 end
